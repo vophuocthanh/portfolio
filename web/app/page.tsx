f8a1bd4aba897ad5138db4689cd8bfdf12d7f4d9
ThanhDev <redheart03@>
2024-01-10 23:34:54 +0700

    feat(feat-footer): add view footer

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,15 +1,17 @@
 import { Banner } from '@/components/banner/banner';
+import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
 import Introduction from '@/components/introduction/introduction';
 import Project from '@/components/project/project';
 
 export default function Home() {
   return (
-    <main className='w-full h-[300rem]'>
+    <main className='w-full h-full'>
       <Header />
-      <Banner></Banner>
+      <Banner />
       <Introduction />
       <Project />
+      <Footer />
     </main>
   );
 }
